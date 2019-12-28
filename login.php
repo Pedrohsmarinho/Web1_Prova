1114fdf777335bd19bf0478a21d6bff3b7bdad93
pphenriquesm <pphenriue543@gmail.com>
2019-12-28 01:21:07 -0300

    login

--- login.php
+++ login.php
@@ -2,26 +2,20 @@
 include_once 'includes/header.php';
 include 'Banco/conect.php';
 include 'Init/init.php';
-
-$smt = $con-> prepare("SELECT email  FROM users WHERE email = ? AND password = ?");
-$smt -> bindParam(1,$email);
-$smt -> bindParam(2, sha1($senha));
-$smt -> execute();
-$resul = $smt -> fetchAll();
-
 ?>
 <div class="row">
 <div class="col s12 m6 push-m3"> 
 <h3 class="light">Logar</h3>
 
-    <form action="index.php" method="POST">
-    <div class="input-field col s12">
-				<input type="text" name="email" id="email">
-				<label for="email">email</label>
-			 </div>
-    <div class="input-field col s12">
-				<input type="password" name="passord" id="passord">
-				<label for="passord">senha</label>
+    <form action="log.php" method="POST">
+<div class="input-field col s12">
+			<input type="text" name="email" id="email">
+			<label for="email">email</label>
+	</div>
+
+<div class="input-field col s12">
+			<input type="password" name="password" id="password">
+			<label for="password">senha</label>
 </div>
 
 <button  type="submit" name="add"class="btn">logar</button>
