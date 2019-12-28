f0860dd1fd999c03f1776cc5fec1b44a4dea27ad
pphenriquesm <pphenriue543@gmail.com>
2019-12-28 01:21:31 -0300

    registro de usuarios

--- reg_user.php
+++ reg_user.php
@@ -5,19 +5,23 @@ echo "<pre>";
     print_r($data);
 echo "</pre>";
 if($data['password'] == $data['password2']){
-    $stmt2 = $con->prepare("SELECT * FROM users WHERE email = ?" );
-    $stmt2->execute([$data['email']]);
-    $stmt2->fetchAll(PDO::FETCH_ASSOC);
+    $smt2 = $con->prepare("SELECT * FROM users WHERE email = ?" );
+    $smt2->execute([$data['email']]);
+    $smt2->fetchAll(PDO::FETCH_ASSOC);
 
-    if($stmt2->rowCount() == 0){
+    if($smt2->rowCount() == 0){
         try{
-            $stmt = $con->prepare("INSERT INTO users(name, email, password) VALUES (?, ?, ?)");
-            $stmt->execute([$data['name'], $data['email'], $data['password']]);
-            header('location: index.php?');
+            $smt = $con->prepare("INSERT INTO users(name, email, password) VALUES (?, ?, ?)");
+            $smt -> bindParam(1, $data['name']);
+            $smt -> bindParam(2, $data['email']);
+            $smt -> bindParam(3, sha1($data['password']));
+            $smt -> execute();
+            $test = $smt -> fetchAll();
         }catch(PDOException $e){
             echo $e;
         }
     }else{
-        echo "usuário already registered";
+        echo "usuário já cadastrado";
     }
+header('location: index.php?');
 }
