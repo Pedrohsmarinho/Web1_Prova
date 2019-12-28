4d0c700da6b02e9d01e023a7a86eda7eb8553ebd
pphenriquesm <pphenriue543@gmail.com>
2019-12-28 01:21:24 -0300

    registro de series

--- reg_series.php
+++ reg_series.php
@@ -1,24 +1,25 @@
 <?php
 include_once "Banco/conect.php";
 $data = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-    $stmt2 = $con->prepare("SELECT * FROM series = ?" );
-    $stmt2->execute([$data]);
-    $stmt2->fetchAll(PDO::FETCH_ASSOC);
+    $smt = $con->prepare("SELECT * FROM series = ?" );
+    $smt->execute([$data]);
+    $smt->fetchAll(PDO::FETCH_ASSOC);
 
-    if($stmt2->rowCount() == 0){
+    if($smt->rowCount() == 0){
         try{
-            $stmt = $con->prepare("INSERT INTO series(name, genre ,  seasons, synopsis) VALUES (?, ?, ?, ?)");
-            $stmt->execute([$data['name'], $data['genre'], $data[' seasons,'], $data['synopsis']]);
-            // var_dump($data);
+            $smt = $con->prepare("INSERT INTO series(name, genre ,  seasons, synopsis) VALUES (?, ?, ?, ?)");
+            $smt -> bindParam(1, $data['name']);
+            $smt -> bindParam(2, $data['genre']);
+            $smt -> bindParam(3, $data['seasons']);
+            $smt -> bindParam(4, $data['synopsis']);
+            $smt -> execute();
+            $series = $smt -> fetchAll();
 
-            
      header('location: index.php?id=Sucesso');
-
-
         }catch(PDOException $e){
             echo $e;
         }
     }else{
-        echo "Series already registered";
+        echo "Series já cadastrada";
     }
  
