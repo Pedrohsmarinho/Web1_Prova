a47cad9db5056e0f3079d4ae531ba7fab1c121ee
pphenriquesm <pphenriue543@gmail.com>
2019-12-29 05:25:57 -0300

    o update

---
+++ update.php
@@ -0,0 +1,27 @@
+<?php 
+//this add the header of the page 
+include_once 'includes/header.php';
+
+// this add the database on system
+include  'Banco/conect.php';
+
+if(!empty($_POST['id'])){
+
+    try {
+        
+    $smt = $con->prepare("UPDATE series SET name= ? , genre= ?, seasons= ?, synopsis= ?  WHERE id = ?");
+    $smt->bindParam(1,$_POST['name']);
+    $smt->bindParam(2,$_POST['genre']);
+    $smt->bindParam(3,$_POST['seasons']);
+    $smt->bindParam(4,$_POST['synopsis']);
+    $smt->bindParam(5,$_POST['id']);
+    $smt->execute();
+    $smt->fetch();
+   // echo $smt->rowCount();
+} catch (PDOException $e){
+            echo $e;
+}
+   header('location:index.php');
+}
+
+?>
