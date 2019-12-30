da51ca076c5564dac9d2b767abb56a6e2b402dfa
pphenriquesm <pphenriue543@gmail.com>
2019-12-30 03:01:33 -0300

    login

--- login.php
+++ login.php
@@ -3,6 +3,21 @@ include_once 'includes/header.php';
 include 'Banco/conect.php';
 include 'Init/init.php';
 ?>
+
+<nav>
+     <div class="nav-wrapper depp black">
+      <a href="index.php" class="brand-logo">ParziFlix</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <?php if(logado()): ?>
+        <li><a href="sair.php">Sair</a></li>
+        <?php else: ?>
+        <li><a href="addUser.php">Cadastre-se</a></li>
+        <li><a href="login.php">Logar</a></li>
+      <?php endif ?>
+      </ul>
+    </div>
+</nav>
+
 <div class="row">
 <div class="col s12 m6 push-m3"> 
 <h3 class="light">Logar</h3>
