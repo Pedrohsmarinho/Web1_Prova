1f60ee0f8011fe15172d7f8179656b8c801ce135
pphenriquesm <pphenriue543@gmail.com>
2019-12-30 02:58:18 -0300

    adicionar Series

--- addSeries.php
+++ addSeries.php
@@ -4,7 +4,25 @@ include_once 'includes/header.php';?>
 <?php 
 // this add the database on system
 include  'Banco/conect.php';
+
+include 'Init/init.php';
 ?>
+
+<nav>
+    <div class="nav-wrapper depp black">
+      <a href="index.php" class="brand-logo">ParziFlix</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <?php if(logado()): ?>
+        <!-- <li><a href="dontwatch.php">Remover Serie</a></li> -->
+        <li><a href="sair.php">Sair</a></li>
+        <?php else: ?>
+        <li><a href="addUser.php">Cadastre-se</a></li>
+        <li><a href="login.php">Logar</a></li>
+      <?php endif ?>
+      </ul>
+    </div>
+  </nav>
+
 <div class="row">
 	<div class="col s12 m6 push-m3"> 
 	<h3 class="light"> Nova Serie</h3>
@@ -26,7 +44,7 @@ include  'Banco/conect.php';
 				<label for="synopsis">Sinópse</label>
 			</div>
 			<button  type="submit" class="btn">Adicione uma nova série</button>
-			<a href="index.php" class="btn green">Home</a>
+			<!-- <a href="index.php" class="btn green">Home</a> -->
 		</form>
 	</div>
 </div> 
