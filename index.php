207268f3910aaae90936dc5f600c4df3e0d1b544
pphenriquesm <pphenriue543@gmail.com>
2019-12-30 03:01:07 -0300

    editando a index

--- index.php
+++ index.php
@@ -8,14 +8,26 @@ include_once 'includes/header.php';
 include 'Init/init.php';
 ?>
 
-<div class="row">
-  <div class="col s12 m6 push-m3">
-            <h3 class="light">Ranking da Semana</h3>
-       <table class="striped">
+  <nav>
+    <div class="nav-wrapper depp black">
+      <a href="index.php" class="brand-logo">ParziFlix</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <?php if(logado()): ?>
+        <li><a href="addSeries.php">Adicionar Series</a></li>
+        <li><a href="atualizar.php">Editar Cadastro</a></li>
+        <li><a href="sair.php">Sair</a></li>
+        <?php else: ?>
+        <li><a href="addUser.php">Cadastre-se</a></li>
+        <li><a href="login.php">Logar</a></li>
+      <?php endif ?>
+      </ul>
+    </div>
+  </nav>
+<div class="col s14 m7">
+            <h3 class="center">Ranking da Semana</h3>
+       <table class="centered">
         <thead>
           <tr>
-             
-              
               <th>Series Name:</th>
               <th>Genre:</th>
               <th>Seasons:</th>
@@ -25,7 +37,7 @@ include 'Init/init.php';
 
         <tbody>
           <?php 
-            $results= $con->prepare("SELECT * FROM series");
+            $results= $con->prepare('SELECT *, (select count(*) from users_series where serie_id = series.id) count_users from series order by count_users desc, name');
             $results->execute();
             $control = $results->fetchAll(PDO::FETCH_ASSOC);
             // var_dump($control);
@@ -49,7 +61,7 @@ include 'Init/init.php';
             </td>          
             <td>
               <?php if(logado()): ?>
-              <a href="dontwatch.php?id=<?php echo $test['id']?>" class="btn-floating red"> <i class="material-icons">remove</i></a>
+              <a href="dontwatch.php?id=<?php echo $test['id']?>" class="btn-floating red"> <i class="material-icons">visibility_off</i></a>
               </td>
           <?php endif;?>
 
@@ -59,16 +71,17 @@ include 'Init/init.php';
 
         </tbody>
       </table>
-      <?php if(logado()): ?>
+<!--       <?php //if(logado()): ?>
             
             <a href="addSeries.php" class="btn">Adicione sua Série</a>
             <a href="sair.php" class="btn">Sair</a>
-      <?php else:?>
+      <?php //else:?>
             <a href="addUser.php" class="btn">Registre-se</a>
             <a href="login.php" class="btn">Logar</a>
-          <?php endif;?>
+          <?php //endif;?> -->
   </div>
 </div>
+
 <?php 
 //footer
 include_once 'includes/footer.php'; 
